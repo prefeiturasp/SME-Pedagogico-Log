deb737cc5565eb96e3850781b16ad6bb67bd1905
Daniel Matsumoto <daniel.matsumoto@amcom.com.br>
2019-04-06 21:24:52 -0300

    [Staged] - SmeLogContext - LogManager

---
+++ SME.Pedagogico.Log/Contexts/SmeLogContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SME.Pedagogico.Log.Contexts
+{
+    public class SmeLogContext : DbContext
+    {
+        #region ==================== ATTRIBUTES ====================
+
+        private string connectionString = "Server=localhost;Port=5432;Database=smeLogDB;Username=postgres;Password=39162604;";
+
+        public DbSet<Models.Action> Actions { get; set; }
+        public DbSet<Models.Application> Applications { get; set; }
+        public DbSet<Models.Connection> Connections { get; set; }
+        public DbSet<Models.Entry> Entries { get; set; }
+        public DbSet<Models.Permission> Permissions { get; set; }
+        public DbSet<Models.User> Users { get; set; }
+
+        #endregion ==================== ATTRIBUTES ====================
+
+        #region ==================== CONSTRUCTORS ====================
+
+        public SmeLogContext()
+            : base()
+        { }
+
+        public SmeLogContext(string connectionString)
+            : base()
+        {
+            this.connectionString = connectionString;
+        }
+
+        #endregion ==================== CONSTRUCTORS ====================
+
+        #region ==================== METHODS ====================
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(connectionString);
+        }
+
+        #endregion ==================== METHODS ====================
+    }
+}
---
+++ SME.Pedagogico.Log/LogManager.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SME.Pedagogico.Log
+{
+    public class LogManager
+    {
+        #region ==================== ATTRIBUTES ====================
+        private Contexts.SmeLogContext db;
+        #endregion ==================== ATTRIBUTES ====================
+
+
+
+        #region ==================== CONSTRUCTORS ====================
+        public LogManager()
+        {
+            db = new Contexts.SmeLogContext();
+        }
+
+        public LogManager(string connectionString)
+        {
+            db = new Contexts.SmeLogContext(connectionString);
+        }
+        #endregion ==================== CONSTRUCTORS ====================
+
+
+
+        #region ==================== METHODS ====================
+        // Criar método genérico para buscar objetos pelo código
+
+        public bool AddEntry(string description, string actionCode, string applicationCode, string ip, string machineName, string permissionCode, string userId, string username)
+        {
+            try
+            {
+                Models.Entry entry = new Models.Entry();
+                entry.NewId();
+                entry.Description = description;
+
+                entry.Action =
+                    (from action in db.Actions
+                     where action.Code == actionCode
+                     select action).FirstOrDefault();
+
+                if (entry.Action == null)
+                {
+                    entry.Action = new Models.Action();
+                    entry.Action.NewId();
+                    entry.Action.Code = actionCode;
+                }
+
+                entry.Application =
+                    (from application in db.Applications
+                     where application.Code == applicationCode
+                     select application).FirstOrDefault();
+
+                if (entry.Application == null)
+                {
+                    entry.Application = new Models.Application();
+                    entry.Application.NewId();
+                    entry.Application.Code = applicationCode;
+                }
+
+                entry.Connection =
+                    (from connection in db.Connections
+                     where connection.IP == ip
+                     && connection.MachineName == machineName
+                     select connection).FirstOrDefault();
+
+                if (entry.Connection == null)
+                {
+                    entry.Connection = new Models.Connection();
+                    entry.Connection.NewId();
+                    entry.Connection.IP = ip;
+                    entry.Connection.MachineName = machineName;
+                }
+
+                entry.Permission =
+                    (from permission in db.Permissions
+                     where permission.Code == permissionCode
+                     select permission).FirstOrDefault();
+
+                if (entry.Permission == null)
+                {
+                    entry.Permission = new Models.Permission();
+                    entry.Permission.NewId();
+                    entry.Permission.Code = actionCode;
+                }
+
+                entry.User =
+                    (from user in db.Users
+                     where user.Id == userId
+                     select user).FirstOrDefault();
+
+                if (entry.User == null)
+                {
+                    entry.User = new Models.User();
+                    entry.User.NewId();
+                    entry.User.UserId = userId;
+                    entry.User.Username = username;
+                }
+
+                db.SaveChanges();
+
+                return (true);
+            }
+            catch (Exception error)
+            {
+                return (false);
+            }
+        }
+        #endregion ==================== METHODS ====================
+    }
+}
